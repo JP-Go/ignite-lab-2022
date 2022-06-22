223ba27bdd80f2952c315d1813cd35cb0ba1a797
João Pedro <jpedro@gmail.com>
2022-06-22 18:56:44 -0300

    Integração dos componentes criados com a API GraphQL

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,48 @@
-import Lesson from "./Lesson";
+import { gql, useQuery } from '@apollo/client';
+import Lesson from './Lesson';
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      availableAt
+      slug
+      title
+      lessonType
+    }
+  }
+`;
+
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string;
+    title: string;
+    slug: string;
+    availableAt: string;
+    lessonType: 'live' | 'class';
+  }[];
+}
 
 export default function Sidebar() {
-	return (<aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
-		<span className="font-bold text-xl pb-6 mb-6 border-b border-gray-500 block">{"Cronograma de aulas"}</span>
-		<div className="flex flex-col gap-8">
-			<Lesson />
-			<Lesson />
-			<Lesson />
-			<Lesson />
-		</div>
-	</aside>);
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+  return (
+    <aside className='w-[348px] bg-gray-700 p-6 border-l border-gray-600'>
+      <span className='font-bold text-xl pb-6 mb-6 border-b border-gray-500 block'>
+        {'Cronograma de aulas'}
+      </span>
+      <div className='flex flex-col gap-8'>
+        {data?.lessons.map(({ id, title, slug, availableAt, lessonType }) => {
+          return (
+            <Lesson
+              key={id}
+              title={title}
+              slug={slug}
+              availableAt={new Date(availableAt)}
+              type={lessonType}
+            />
+          );
+        })}
+      </div>
+    </aside>
+  );
 }
