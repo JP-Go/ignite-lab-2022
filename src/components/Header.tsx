d72598a9e9e83375dcd330bb26333aee07782132
João Pedro <jpedro@gmail.com>
2022-06-22 10:39:44 -0300

    Criação do cabeçalho da página do evento

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,7 @@
+import Logo from "./Logo";
+
 export default function Header() {
-	return (<h1>Header</h1>);
+	return (<header className={"flex justify-center items-center py-5 w-full bg-gray-700 border-b border-gray-600"}>
+		<Logo />
+	</header>);
 }
