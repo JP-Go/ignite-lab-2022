67736f15822f47d07d7830b77d59ec4fed5fadcf
João Pedro <jpedro@gmail.com>
2022-06-22 01:35:46 -0300

    Criação dos componentes básicos da aplicação. Boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
-import { gql, useQuery } from "@apollo/client";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
-
-interface LessonsData {
-  lessons: Lesson[];
-}
+import Evento from "./pages/Evento";
 
 function App() {
-  const { data } = useQuery<LessonsData>(GET_LESSONS_QUERY);
-
-  return (
-    <ul>
-      {data?.lessons.map(({ id, title }: Lesson) => {
-        return <li key={id}>{title}</li>;
-      })}
-    </ul>
-  );
+	return <Evento/>
 }
 
 export default App;
