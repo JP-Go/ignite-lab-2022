5fe983a9637dc0908480641fda53f541144f6784
João Pedro <jpedro@gmail.com>
2022-06-22 01:37:08 -0300

    Uso da minha própria rota do GraphCMS

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,9 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const token = 'cl4ljcdt56mom01yrdswgf7n9'
-
 export const client = new ApolloClient({
-	//uri: `https://api-sa-east-1.graphcms.com/v2/${token}/master`,
-	uri: `https://api-sa-east-1.graphcms.com/v2/cl4ljcdt56mom01yrdswgf7n9/master`,
+	uri: "https://api-sa-east-1.graphcms.com/v2/cl4o5l9qc0pff01xm22sohz4c/master",
 	cache: new InMemoryCache()
 })
