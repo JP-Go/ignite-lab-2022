51950177082ff6984ca0105c68d0f3e2b055affa
João Pedro <jpedro@gmail.com>
2022-06-24 01:10:40 -0300

    Introdução de roteamento na página do evento para os vídeos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import Event from './pages/Event';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { client } from './lib/apollo';
+import { Router } from './Router';
 
 function App(): JSX.Element {
-  return <Event />;
+  //return <Event />;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
